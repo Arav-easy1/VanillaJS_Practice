566c9ab2e9cf74026e75ab74944eea996e36a355
EASY_ARAV <wg0418@gmail.com>
2020-07-25 17:07:17 +0900

    document.querySelector 많이 쓴댜요

---
+++ 2020_07_25/index.js
@@ -0,0 +1,7 @@
+// const title = document.getElementById("title");
+// const title = document.querySelector(".title"); // 클래스로 찾고 싶으면
+const title = document.querySelector("#title"); // id로 찾고 싶으면
+
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "I own you now";
